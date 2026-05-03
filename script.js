97fcc3c52cc62c2671f1ba49fc7e2d1a61b98d9d
saitejasiripuram987sst <saitejasiripuram987@gmail.com>
2026-05-03 13:08:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value;
+
+  if (task === "") return;
+
+  let li = document.createElement("li");
+  li.textContent = task;
+
+  li.onclick = function () {
+    li.style.textDecoration = "line-through";
+  };
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
